20afbe464716b44d25a86f6ff3443bbcd21f92aa
nikolaSherperd <nikola07718@gmail.com>
2025-11-26 11:00:34 +0100

    Reapply "Made MOdifications"
    This reverts commit ea8a4d5b62d83ca1a389f6a1bce57d5ee0900c39.

---
+++ variable.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main(){
+
+    /* Variables;
+     * Variables = A reusable container for storing data values.
+     * A variable behaves like the value it holds.
+     */
+
+	int age = 25;
+
+	printf("You are %d years old\n", age);
+
+	return 0;
+     
+}
