37a776132b07d4756837a199f4ec15aa912b5a54
nikolaSherperd <nikola07718@gmail.com>
2025-11-26 10:57:46 +0100

    Reapply "Outputting text to the console"
    This reverts commit 687adc36c5264e651a74899fc1118b9fd915b71b.

---
+++ main.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+
+//outputting text to the console
+
+
+	printf("And thus the heart will break\n"
+			"yet brokenly live on\n");
+                  
+        return 0;
+
+}
