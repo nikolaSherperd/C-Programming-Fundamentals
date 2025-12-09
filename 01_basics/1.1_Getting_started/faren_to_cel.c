88a972c94533056636203f8ca86289dea4324ef1
nikolaSherperd <nikola07718@gmail.com>
2025-12-09 19:35:53 +0100

    New file Added

---
+++ 01_basics/1.1_Getting_started/faren_to_cel.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* The program converts Fahrenheit to 
+ * celsius and prints thier equal 
+ * equivalent in tabular format
+ *
+ *
+ * Fahrenheit      Celsius
+ * 1               -17
+ * 20              -6
+ * 40              4
+ * 60              15 
+ * 100             26
+ * 120             37 
+ * 140             48
+ * 160		   60
+ * 180		   71
+ * 200		   82
+ * 220             93
+ * 240             104
+ * 260             115
+ * 280             126
+ * 300             137*/
+
+int main(){
+
+
+
+
+}
