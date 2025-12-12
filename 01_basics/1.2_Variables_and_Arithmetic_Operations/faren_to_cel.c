f9565e53780c7951e7ca0c0633c5ee1374f74561
nikolaSherperd <nikola07718@gmail.com>
2025-12-12 23:53:08 +0100

    Moved Files to New Directory

---
+++ 01_basics/1.2_Variables_and_Arithmetic_Operations/faren_to_cel.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/* The program converts Fahrenheit to 
+ * celsius and prints thier equal 
+ * equivalent in tabular format
+ *
+ *
+ * Fahrenheit      Celsius
+ * 1               -17
+ * 20              -6
+ * 40              4
+ * 60              15 
+ * 100             26
+ * 120             37 
+ * 140             48
+ * 160		   60
+ * 180		   71
+ * 200		   82
+ * 220             93
+ * 240             104
+ * 260             115
+ * 280             126
+ * 300             137*/
+
+int main()
+{
+
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;    //Lower limit on Temperature scale
+  upper = 300; //Upper limit on Temperature scale
+  step  = 20; //step Size
+  
+
+
+}
