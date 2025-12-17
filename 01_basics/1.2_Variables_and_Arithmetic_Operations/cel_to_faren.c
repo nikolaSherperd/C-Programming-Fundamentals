a5770b23b385e2d0848a7948a5d6feecce6cf98e
nikolaSherperd <nikola07718@gmail.com>
2025-12-17 15:40:24 +0100

    ModficationS Made

---
+++ 01_basics/1.2_Variables_and_Arithmetic_Operations/cel_to_faren.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+
+  float fahr,  celsius;
+  float lower, upper, step;
+
+  lower = -17.8    /* Lower Limit of Temperature Scale */
+  upper = 148.9	  /*  Upper Limit of Temperature Scale */
+  step  = 11.1    /* Step Size */
+
+  celsius = lower;
+  while (celsius)
+ 
+
+
+}
--- 01_basics/1.2_Variables_and_Arithmetic_Operations/faren_to_cel.c
+++ 01_basics/1.2_Variables_and_Arithmetic_Operations/faren_to_cel.c
@@ -1,42 +1,54 @@
 #include <stdio.h>
-/* The program converts Fahrenheit to 
+/*.c -o build/basics
+ * Types_Operators_Expression:
+ * $(CC)  $(CFLAGS) 02_Types_Operators_Expression
+ * The program converts Fahrenheit to 
  * celsius and prints thier equal 
  * equivalent in tabular format
  *
  *
  * Fahrenheit      Celsius
- * 1               -17
- * 20              -6
- * 40              4
- * 60              15 
- * 100             26
- * 120             37 
- * 140             48
- * 160		   60
- * 180		   71
- * 200		   82
- * 210             93
- * 220             104
- * 240             115
- * 260             126
- * 280             137
- * 300             148*/
+ *  0  -17.8
+ *  20   -6.7
+ *  40    4.4
+ *  60   15.6
+ *  80   26.7
+ * 100   37.8
+ * 120   48.9
+ * 140   60.0
+ * 160   71.1
+ * 180   82.2
+ * 200   93.3
+ * 220  104.4
+ * 240  115.6
+ * 260  126.7
+ * 280  137.8
+ * 300  148.9*/
+
 
 int main()
 {
-
-  int fahr, celsius;
-  int lower, upper, step;
-
-  lower = 0;    //Lower limit on Temperature scale
-  upper = 300; //Upper limit on Temperature scale
-  step  = 20; //step Size
   
+  char* Title;
+
+  float fahr, celsius;                                   // Use of Float aritmetic for 
+			                                // better precision.
+  float lower, upper, step;
+  lower = 0;                                           //Lower limit on Temperature scale
+  upper = 300;                                        //Upper limit on Temperature scale
+  step  = 20;                                        //step Size
+  
+  Title = 'Temperature Conversion Table';
   fahr = lower;
   while (fahr <= upper) {
 
-	  celsius = 5 * (fahr-32) / 9;
-	  printf("%d\t%d\n", fahr, celsius);
+	  celsius = 5.0 / 9.0 * (fahr-32.0);
+	  printf("%c\n", Title);
+	  printf("%3.0f %6.1f\n", fahr, celsius); // (%3d %6) Prints the 
+					         // first number of each
+					        // line in a field, three
+					       // digits wide and the second 
+					      // six digits wide.
 	  fahr = fahr + step;
   }
 
