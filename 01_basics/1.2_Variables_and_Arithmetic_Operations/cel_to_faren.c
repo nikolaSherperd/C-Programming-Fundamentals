0f65940a756a370af4a161f71ead448c5c6a5b63
nikolaSherperd <nikola07718@gmail.com>
2025-12-18 16:01:37 +0100

    Made Modifications

--- 01_basics/1.2_Variables_and_Arithmetic_Operations/cel_to_faren.c
+++ 01_basics/1.2_Variables_and_Arithmetic_Operations/cel_to_faren.c
@@ -3,16 +3,20 @@
 int main ()
 {
 
-  float fahr,  celsius;
+  float celsius,  fahr;
   float lower, upper, step;
 
-  lower = -17.8    /* Lower Limit of Temperature Scale */
-  upper = 148.9	  /*  Upper Limit of Temperature Scale */
-  step  = 11.1    /* Step Size */
+  upper = -17.8;    /* Lower Limit of Temperature Scale */
+  lower = 148.9;    /*  Upper Limit of Temperature Scale */
+  step  = 11.1;     /* Step Size */
 
-  celsius = lower;
-  while (celsius)
+  celsius = upper;
+  while (celsius <= lower) {
+      fahr = (9.0 / 5.0) * (celsius + 32);
+      printf("%3.0f %6.1f\n", fahr, celsius);
+      
+      celsius = celsius + step;
+
+  }
  
-
-
 }
