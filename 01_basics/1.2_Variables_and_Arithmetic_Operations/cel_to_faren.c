c037c06c653dc1c78a281c87e13424e61f61679a
nikolaSherperd <nikola07718@gmail.com>
2026-04-19 23:59:47 +0100

    modified: cel_tofaren.c

--- 01_basics/1.2_Variables_and_Arithmetic_Operations/cel_to_faren.c
+++ 01_basics/1.2_Variables_and_Arithmetic_Operations/cel_to_faren.c
@@ -9,11 +9,13 @@ int main ()
   upper = -17.8;    /* Lower Limit of Temperature Scale */
   lower = 148.9;    /*  Upper Limit of Temperature Scale */
   step  = 11.1;     /* Step Size */
+                                                
+  printf("****TEMPERATURE CONVERSION TABLE*****\n");
 
   celsius = upper;
   while (celsius <= lower) {
       fahr = (9.0 / 5.0) * (celsius + 32);
-      printf("%3.0f %6.1f\n", fahr, celsius);
+      printf("%3.2f %6.1f\n", fahr, celsius);
       
       celsius = celsius + step;
 
